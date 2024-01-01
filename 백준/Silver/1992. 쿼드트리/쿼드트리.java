caf6570496644a247827a81d0bd467e5d7c36645
Park-Jaehyeon98 <78424655+Park-Jaehyeon98@users.noreply.github.com>
2024-01-01 21:02:20 +0900

    [Silver I] Title: 쿼드트리, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -27,26 +27,24 @@ public class Main {
 
 	public static void QuadTree(int x, int y, int size) {
 
-		// 압축이 가능할 경우 하나의 색상으로 압축해준다.
 		if (isPossible(x, y, size)) {
 			sb.append(img[x][y]);
 			return;
 		}
 
-		int newSize = size / 2; // 압축이 불가능 할 경우 사이즈를 절반으로 나누어야 한다.
+		int newSize = size / 2; 
 
-		sb.append('('); // 각 레벨(depth)에서 여는괄호로 시작해야한다.
+		sb.append('('); 
 
 		QuadTree(x, y, newSize); // 왼쪽 위
 		QuadTree(x, y + newSize, newSize); // 오른쪽 위
 		QuadTree(x + newSize, y, newSize); // 왼쪽 아래
 		QuadTree(x + newSize, y + newSize, newSize); // 오른쪽 아래
 
-		sb.append(')'); // 해당 레벨이 끝나면 닫는괄호도 닫아준다.
+		sb.append(')');
 
 	}
 
-	// 압축이 가능한지 해당 공간을 체크해주는 함수
 	public static boolean isPossible(int x, int y, int size) {
 		int value = img[x][y];
 
